7bafca5818e5f58db7f84d140226ca47408eb3ed
rhoedhi <39488656+rhoedhi@users.noreply.github.com>
2018-08-08 12:43:14 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# itsR03D17w7
+#itsR03D17w7 v 1.0
+
+Bismillah. Alhamdulillah tools ini akhirnya selesai.
+Terimakasi atas saran dan masukan kalian semua, tampa saran dan masukan kalian tools ini gak selesai...!!!
+
+Thank's to: Mr.w4hyu | TUAN B4DUT | ~itsZNine7⁩ | ⁨itsMyth7w7⁩ | R4M4_P353K | ~Mrs. Swaath | ⁨~Tn. Chaplin | All Admin and Member PANDA7w7
+
+Jika tools ini ingin di Recode! Gue nitip nick  dan semua yang gue cantumkan disini, karena itu semua bagian dari gue...!!!
+
+Note:
+          Sebelum di Recode, alangkah baiknya hubungi gue dulu gan:v......!!!
+Email:
+           rhoedhi33@gmail.com
+
+Gunakanlah tools ini dengan bijak..!!!
+Sekian dan terimakasih salam dari saya Noob yang tak di anggap:v   wkwkwk
---
+++ itsR03D17w7.sh
@@ -0,0 +1,436 @@
+#!/system/xbin/bash
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f slant -F gay "Install Tools itsR03D17w7" | lolcat
+echo "\033[35;1m"
+cal
+date
+echo ""
+echo "\033[32;1mAuthor : itsR03D17w7"
+echo "\033[33;1mKontak Me :  rhoedhi33@gmail.com"
+echo "\033[31;1mGitHub: https://github.com/rhoedhi"
+echo "\033[33;1mPANDA7w7" "\033[32;1m&" "\033[36;1m70MBL0 G4N5 CR3W"
+echo "\033[34;1m[============================]>"
+sleep 1
+echo ""
+echo "\033[32;1mPilih Toolsnya Dan Gunakanlah Dengan Bijak:)?:"
+echo "\033[36;1m"
+echo "[===============================================]>"
+echo "01.> Install Creadmap-check login emails"
+echo "[===============================================]>"
+echo "02.> Install Click-Jacking Scanner"
+echo "[===============================================]>"
+echo "03.> Install Check url"
+echo "[===============================================]>"
+echo "04.> Install Weevely Backdoor-webshell"
+echo "[===============================================]>"
+echo "05.> Install Cyber-scan"
+echo "[===============================================]>"
+echo "06.> Install A2SV ssl-vul-scan"
+echo "[===============================================]>"
+echo "07.> Install smb-scanner"
+echo "[===============================================]>"
+echo "08.> Install Nikto web server scanner"
+echo "[===============================================]>"
+echo "09.> Install Termux Lazy-script"
+echo "[===============================================]>"
+echo "10.> Install D-TECT"
+echo "[===============================================]>"
+echo "11.> Install Commix"
+echo "[===============================================]>"
+echo "12.> Install Brutal"
+echo "[===============================================]>"
+echo "13.> Install SocialFish For All Pishing"
+echo "[===============================================]>"
+echo "14.> Install Golden Eye-Dos tool"
+echo "[===============================================]>"
+echo "15.> Install Hulk - Dos-Tool"
+echo "[===============================================]>"
+echo "16.> Install Termux-ubuntu"
+echo "[===============================================]>"
+echo "17.> Install Infoga-collect email-info"
+echo "[===============================================]>"
+echo "18.> Install cms map vul-scanner"
+echo "[===============================================]>"
+echo "19.> Install IPGeoLocation-Trace ip"
+echo "[===============================================]>"
+echo "20.> Install Termux-Theme"
+echo "[===============================================]>"
+echo "21.> Install Striker"
+echo "[===============================================]>"
+echo "0.> keluar"
+echo "[===============================================]>"
+read -p "root@InstallTools-itsR03D17w7]~#" bro
+
+if [ $bro = 01 ] || [ $bro = 01 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ apt install python
+ git clone https://github.com/lightos/credmap
+ cd creadmap
+ chmod +x *
+ python2 credmap.py -h --username"[nama lu]" --email"[email lu]"
+fi
+
+if [ $bro = 02 ] || [ $bro = 02 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git
+ apt install python2
+ apt install python
+ git clone https://github.com/D4Vinci/Clickjacking-Tester
+ cd Clickjacking-Tester
+ chmod +x *
+ python3 Clickjacking-Tester.py file.txt
+fi
+
+if [ $bro = 03 ] || [ $bro = 03 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ apt install python
+ git clone https://github.com/UndeadSec/checkURL
+ cd checkURL
+ chmod +x *
+ python3 checkURL.py --help
+ python3 checkURL.py --url "[attackerurl]"
+fi
+
+if [ $bro = 04 ] || [ $bro = 04 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ git clone https://github.com/glides/Weevely
+ cd Weevely
+ chmod +x *
+ python2 weevely.py
+ python2 weevely.py generate 123456 /$HOME/shell.php
+fi
+
+if [ $bro = 05 ] || [ $bro = 05 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ apt install python
+ git clone https://github.com/medbenali/CyberScan.git
+ cd CyberScan
+ python2 CyberScan.py -v
+ CyberScan -h
+ python2 CyberScan -s 192.168.1.0/24 -p arp
+ CyberScan -s "[ip target]" -p tcp -d 80
+fi
+
+if [ $bro = 06 ] || [ $bro = 06 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ apt install python
+ git clone https://github.com/hahwul/a2sv
+ cd a2sv
+ chmod +x *
+ pip2 install -r requirements.txt
+ python2 a2sv.py -h
+ python a2sv.py -t "[target means here own device]"
+fi
+
+if [ $bro = 07 ] || [ $bro = 07 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ apt install python
+ git clone https://github.com/TechnicalMujeeb/smb-scanner
+ cd smb-scanner
+ cd modules
+ chmod +x *
+ cd ..
+ chmod +x *
+ ./install.sh
+ ./smbscan
+fi
+
+if [ $bro = 08 ] || [ $bro = 08 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install perl
+ git clone https://github.com/sullo/nikto
+ cd nikto
+ chmod +x *
+ perl nikto.pl -H
+fi
+
+if [ $bro = 09 ] || [ $bro = 09 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ git clone https://github.com/TechnicalMujeeb/Termux-Lazyscript.git
+ cd Termux-Lazyscript
+ chmod +x *
+ sh setup.sh
+ python2 ls.py
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ git clone https://github.com/shawarkhanethicalhacker/D-TECT
+ cd D-TECT
+ chmod +x *
+ pip2 install requests
+ python2 d-tect.py
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ git clone https://github.com/commixproject/commix
+ cd commix
+ chmod +x *
+ python2 commix.py
+ python2 commix.py -h 
+ python2 commix.py -u "[site.com]"
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ git clone https://github.com/Screetsec/Brutal
+ cd Brutal
+ chmod +x *
+ sudo ./Brutal.sh
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git
+ apt install python2
+ git clone https://github.com/UndeadSec/SocialFish.git
+ cd SocialFish
+ chmod +x *
+ pip2 install -r requirements.txt
+ python2 SocialFish.py
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ git clone https://github.com/jseidl/GoldenEye
+ cd GoldenEye
+ chmod +x *
+ python2 goldeneye.py "[url]"
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ git clone https://github.com/grafov/hulk
+ cd hulk
+ chmod +x *
+ python2 hulk.py "[url]"
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install proot
+ apt install wget
+ git clone https://github.com/Neo-Oli/termux-ubuntu
+ cd termux-ubuntu
+ ls
+ chmod +x *
+ sh ubuntu.sh
+ ./start.sh
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ git clone https://github.com/m4ll0k/Infoga
+ cd Infoga
+ chmod +x *
+ pip2 install requests
+ python2 infoga.py -t "[gmail.com]" -s all
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git
+ apt install python2
+ git clone https://github.com/Dionach/CMSmap.git
+ cd CMSmap
+ chmod +x *
+ python2 cmsmap.py -h
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python
+ git clone https://github.com/maldevel/IPGeoLocation
+ cd IPGeoLocation
+ chmod +x *
+ pip install -r requirements.txt
+ python ipgeolocation.py -t "[target ip]"
+fi
+
+if [ $bro = 20 ] || [ $bro = 20 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ git clone https://github.com/Cabbagec/termux-ohmyzsh
+ cd termux-ohmyzsh
+ chmod +x *
+ ./install.sh
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then
+clear
+cowsay -f kiss.cow "itsR03D17w7" | lolcat
+figlet "itsR03D17w7" | lolcat
+sleep 1
+ apt update && apt upgrade
+ apt install git 
+ apt install python2
+ apt install python
+ git clone https://github.com/s0md3v/Striker
+ cd Striker
+ pip2 install -r requirements.txt
+ python2 striker.py
+fi
+
+if [ $bro = 00 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mWe Are Family DARKNESS CYBER TEAM"
+sleep 1
+echo "\033[33;1mThank's to: Allah SWT - Muhammad SAW"
+sleep 1
+echo "Ayah - Bunda"
+sleep 1
+echo "Mr.w4hyu - fifit.p"
+sleep 1
+echo "Mr.BAJINGAN - TUAN B4DUT"
+sleep 1
+echo "R4M4_P353K -?itsMyth7w7? -?~itsZNine7?"
+sleep 1
+echo "?itsHmei5R7? -?~Miv? -?~Mr. Clay HA? -?~Mrs. Swaath? -~Tn. Chaplin"
+sleep 1
+echo "All Member 70MBL0 G4N5 CR3W"
+sleep 1
+echo "\033[32;1mJulukan Untuk Noob Kayak Gue...."
+sleep 1
+echo "\ Yang Selalu Gue Dapat Dari Para Mastah Adalah [TUKANG_COPAS_LHU_TOD:v.....!!!]"
+sleep 1
+echo "\033[32;1mSalam dari saya, Noob kelas teri.......!!!"
+exit
+fi
